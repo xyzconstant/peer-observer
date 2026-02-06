9d220e7aa8286cb552b8eec7ecd2b2851a704ab7
0xb10c <b10c@b10c.me>
2026-02-06 17:53:08 +0100

    use: GetPeerInfo version specific type explicitly

--- shared/src/protobuf/rpc_extractor.rs
+++ shared/src/protobuf/rpc_extractor.rs
@@ -5,13 +5,14 @@ use corepc_client::types::v17::{
 };
 use corepc_client::types::v26::{
     AddrManInfoNetwork as RPCAddrManInfoNetwork, GetAddrManInfo as RPCGetAddrManInfo,
-    GetPeerInfo as RPCGetPeerInfo, PeerInfo as RPCPeerInfo,
 };
 
 // Types that don't have a generic model type in corepc (yet).
+// Once they have a model, move them down to the mtypes!
+use corepc_client::types::v26::{GetPeerInfo as RPCGetPeerInfo, PeerInfo as RPCPeerInfo};
 use corepc_client::types::v28::{GetRawAddrMan, RawAddrManEntry};
 
-// TODO: Ideally, all type imports should use the generic mtype types.
+// Ideally, all type imports should use the generic mtype types.
 use corepc_node::mtype::{
     GetBlockchainInfo, GetChainTxStats, GetMempoolInfo, GetNetworkInfo, GetNetworkInfoAddress,
     GetNetworkInfoNetwork, GetOrphanTxsVerboseTwo, GetOrphanTxsVerboseTwoEntry,
