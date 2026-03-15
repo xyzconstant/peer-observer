e9f3ccce168c3e102a2d9ad4c94d16e6ecefb3eb
xyzconstant <263061129+xyzconstant@users.noreply.github.com>
2026-03-15 17:50:50 -0300

    feat(ipc-extractor): move IPC communication logic to separate module

---
+++ extractors/ipc/src/ipc.rs
@@ -0,0 +1,121 @@
+use crate::error::{IpcCallKind, RuntimeError};
+use crate::init_capnp::init;
+use crate::mining_capnp::mining;
+use crate::proxy_capnp::thread;
+use capnp_rpc::{RpcSystem, rpc_twoparty_capnp, twoparty};
+use shared::protobuf::ipc_extractor::BlockTip;
+use shared::{
+    futures::AsyncReadExt,
+    tokio::{net::UnixStream, task::JoinHandle},
+};
+
+impl mining::Client {}
+
+pub struct IpcClient {
+    pub mining: mining::Client,
+    pub thread: thread::Client,
+    pub rpc_task: JoinHandle<Result<(), capnp::Error>>,
+}
+
+impl IpcClient {
+    pub async fn init(stream: UnixStream) -> Result<Self, crate::error::RuntimeError> {
+        let (reader, writer) = tokio_util::compat::TokioAsyncReadCompatExt::compat(stream).split();
+        let network = Box::new(twoparty::VatNetwork::new(
+            reader,
+            writer,
+            rpc_twoparty_capnp::Side::Client,
+            Default::default(),
+        ));
+
+        let mut rpc_system = RpcSystem::new(network, None);
+        let init: init::Client = rpc_system.bootstrap(rpc_twoparty_capnp::Side::Server);
+        let rpc_task = shared::tokio::task::spawn_local(rpc_system);
+
+        let response = init
+            .construct_request()
+            .send()
+            .promise
+            .await
+            .map_err(|e| RuntimeError::ipc_call(IpcCallKind::InitConstruct, e))?;
+        let thread_map = response
+            .get()
+            .map_err(|e| RuntimeError::ipc_call(IpcCallKind::InitConstruct, e))?
+            .get_thread_map()
+            .map_err(|e| RuntimeError::ipc_call(IpcCallKind::InitConstruct, e))?;
+
+        let response = thread_map
+            .make_thread_request()
+            .send()
+            .promise
+            .await
+            .map_err(|e| RuntimeError::ipc_call(IpcCallKind::ThreadMapMakeThread, e))?;
+        let thread = response
+            .get()
+            .map_err(|e| RuntimeError::ipc_call(IpcCallKind::ThreadMapMakeThread, e))?
+            .get_result()
+            .map_err(|e| RuntimeError::ipc_call(IpcCallKind::ThreadMapMakeThread, e))?;
+
+        let mut make_mining_request = init.make_mining_request();
+        {
+            let mut context = make_mining_request
+                .get()
+                .get_context()
+                .map_err(|e| RuntimeError::ipc_call(IpcCallKind::InitMakeMining, e))?;
+            context.set_thread(thread.clone());
+            context.set_callback_thread(thread.clone());
+        }
+        let response = make_mining_request
+            .send()
+            .promise
+            .await
+            .map_err(|e| RuntimeError::ipc_call(IpcCallKind::InitMakeMining, e))?;
+        let mining = response
+            .get()
+            .map_err(|e| RuntimeError::ipc_call(IpcCallKind::InitMakeMining, e))?
+            .get_result()
+            .map_err(|e| RuntimeError::ipc_call(IpcCallKind::InitMakeMining, e))?;
+
+        Ok(Self {
+            rpc_task,
+            thread,
+            mining,
+        })
+    }
+
+    pub async fn get_tip(&self) -> Result<BlockTip, RuntimeError> {
+        let mut get_tip_request = self.mining.get_tip_request();
+        {
+            let mut context = get_tip_request
+                .get()
+                .get_context()
+                .map_err(|e| RuntimeError::ipc_call(IpcCallKind::MiningGetTip, e))?;
+            context.set_thread(self.thread.clone());
+            context.set_callback_thread(self.thread.clone());
+        }
+
+        let get_tip_response = get_tip_request
+            .send()
+            .promise
+            .await
+            .map_err(|e| RuntimeError::ipc_call(IpcCallKind::MiningGetTip, e))?;
+
+        let height: i32;
+        let mut hash: Vec<u8>;
+        {
+            let tip = get_tip_response
+                .get()
+                .map_err(|e| RuntimeError::ipc_call(IpcCallKind::MiningGetTip, e))?
+                .get_result()
+                .map_err(|e| RuntimeError::ipc_call(IpcCallKind::MiningGetTip, e))?;
+
+            height = tip.get_height();
+            hash = tip
+                .get_hash()
+                .map_err(|e| RuntimeError::ipc_call(IpcCallKind::MiningGetTip, e))?
+                .to_vec();
+            hash.reverse();
+        }
+
+        Ok(BlockTip { height, hash })
+    }
+}
--- extractors/ipc/src/lib.rs
+++ extractors/ipc/src/lib.rs
@@ -1,20 +1,9 @@
-capnp::generated_code!(pub mod proxy_capnp, "capnp/mp/proxy_capnp.rs");
-capnp::generated_code!(pub mod common_capnp, "capnp/common_capnp.rs");
-capnp::generated_code!(pub mod mining_capnp, "capnp/mining_capnp.rs");
-capnp::generated_code!(pub mod echo_capnp, "capnp/echo_capnp.rs");
-capnp::generated_code!(pub mod init_capnp, "capnp/init_capnp.rs");
-
-use crate::init_capnp::init;
-use crate::mining_capnp::mining;
-use crate::proxy_capnp::thread;
-use capnp_rpc::{RpcSystem, rpc_twoparty_capnp, twoparty};
 use shared::clap::{self, Parser};
-use shared::futures::AsyncReadExt;
 use shared::nats_subjects::Subject;
 use shared::nats_util::{self, NatsArgs};
 use shared::prost::Message;
 use shared::protobuf::event::{Event, event::PeerObserverEvent};
-use shared::protobuf::ipc_extractor::{self, BlockTip};
+use shared::protobuf::ipc_extractor::{self};
 use shared::tokio::net::UnixStream;
 use shared::tokio::sync::watch;
 use shared::tokio::time::{self, Duration};
@@ -22,7 +11,16 @@ use shared::{async_nats, log};
 use std::io;
 
 mod error;
-use error::{IpcCallKind, RuntimeError};
+use error::RuntimeError;
+
+// Generate capnp bindings before using `ipc` module
+capnp::generated_code!(pub mod proxy_capnp, "capnp/mp/proxy_capnp.rs");
+capnp::generated_code!(pub mod common_capnp, "capnp/common_capnp.rs");
+capnp::generated_code!(pub mod mining_capnp, "capnp/mining_capnp.rs");
+capnp::generated_code!(pub mod echo_capnp, "capnp/echo_capnp.rs");
+capnp::generated_code!(pub mod init_capnp, "capnp/init_capnp.rs");
+mod ipc;
+use ipc::IpcClient;
 
 /// The peer-observer ipc-extractor periodically queries data from the
 /// Bitcoin Core IPC interface and publishes the results as events into
@@ -65,19 +63,26 @@ impl Args {
     }
 }
 
-struct IpcSession {
-    mining: mining::Client,
-    thread: thread::Client,
-    rpc_task: shared::tokio::task::JoinHandle<Result<(), capnp::Error>>,
-}
-
 pub async fn run(args: Args, mut shutdown_rx: watch::Receiver<bool>) -> Result<(), RuntimeError> {
     let nats_client = nats_util::prepare_connection(&args.nats)?
         .connect(&args.nats.address)
         .await?;
     log::info!("Connected to NATS server at {}", &args.nats.address);
 
-    let ipc_session = init_ipc_session(&args.ipc_socket_path).await?;
+    let stream = UnixStream::connect(&args.ipc_socket_path)
+        .await
+        .map_err(|e| {
+            io::Error::new(
+                e.kind(),
+                format!(
+                    "could not connect to IPC socket at --ipc-socket-path '{}': {}",
+                    &args.ipc_socket_path, e
+                ),
+            )
+        })?;
+    shared::log::info!("Connected to IPC socket path at {}", &args.ipc_socket_path);
+
+    let ipc_session = IpcClient::init(stream).await?;
 
     let duration_sec = Duration::from_secs(args.query_interval);
     let mut interval = time::interval(duration_sec);
@@ -89,7 +94,7 @@ pub async fn run(args: Args, mut shutdown_rx: watch::Receiver<bool>) -> Result<(
     loop {
         shared::tokio::select! {
             _ = interval.tick() => {
-                if let Err(e) = get_height(&ipc_session, &nats_client).await {
+                if let Err(e) = get_tip(&ipc_session, &nats_client).await {
                         log::error!("Could not fetch and publish 'getHeight': {}", e)
                     }
             }
@@ -115,123 +120,14 @@ pub async fn run(args: Args, mut shutdown_rx: watch::Receiver<bool>) -> Result<(
     Ok(())
 }
 
-async fn init_ipc_session(ipc_socket_path: &str) -> Result<IpcSession, RuntimeError> {
-    let stream = UnixStream::connect(ipc_socket_path).await.map_err(|e| {
-        io::Error::new(
-            e.kind(),
-            format!(
-                "could not connect to IPC socket at --ipc-socket-path '{}': {}",
-                ipc_socket_path, e
-            ),
-        )
-    })?;
-    log::info!("Connected to IPC socket path at {}", ipc_socket_path);
-
-    let (reader, writer) = tokio_util::compat::TokioAsyncReadCompatExt::compat(stream).split();
-    let network = Box::new(twoparty::VatNetwork::new(
-        reader,
-        writer,
-        rpc_twoparty_capnp::Side::Client,
-        Default::default(),
-    ));
-
-    let mut rpc_system = RpcSystem::new(network, None);
-    let init: init::Client = rpc_system.bootstrap(rpc_twoparty_capnp::Side::Server);
-    let rpc_task = shared::tokio::task::spawn_local(rpc_system);
-
-    let response = init
-        .construct_request()
-        .send()
-        .promise
-        .await
-        .map_err(|e| RuntimeError::ipc_call(IpcCallKind::InitConstruct, e))?;
-    let thread_map = response
-        .get()
-        .map_err(|e| RuntimeError::ipc_call(IpcCallKind::InitConstruct, e))?
-        .get_thread_map()
-        .map_err(|e| RuntimeError::ipc_call(IpcCallKind::InitConstruct, e))?;
-
-    let response = thread_map
-        .make_thread_request()
-        .send()
-        .promise
-        .await
-        .map_err(|e| RuntimeError::ipc_call(IpcCallKind::ThreadMapMakeThread, e))?;
-    let thread = response
-        .get()
-        .map_err(|e| RuntimeError::ipc_call(IpcCallKind::ThreadMapMakeThread, e))?
-        .get_result()
-        .map_err(|e| RuntimeError::ipc_call(IpcCallKind::ThreadMapMakeThread, e))?;
-
-    let mut make_mining_request = init.make_mining_request();
-    {
-        let mut context = make_mining_request
-            .get()
-            .get_context()
-            .map_err(|e| RuntimeError::ipc_call(IpcCallKind::InitMakeMining, e))?;
-        context.set_thread(thread.clone());
-        context.set_callback_thread(thread.clone());
-    }
-    let response = make_mining_request
-        .send()
-        .promise
-        .await
-        .map_err(|e| RuntimeError::ipc_call(IpcCallKind::InitMakeMining, e))?;
-    let mining = response
-        .get()
-        .map_err(|e| RuntimeError::ipc_call(IpcCallKind::InitMakeMining, e))?
-        .get_result()
-        .map_err(|e| RuntimeError::ipc_call(IpcCallKind::InitMakeMining, e))?;
-
-    Ok(IpcSession {
-        rpc_task,
-        thread,
-        mining,
-    })
-}
-
-async fn get_height(
-    ipc_client: &IpcSession,
+async fn get_tip(
+    ipc_client: &IpcClient,
     nats_client: &async_nats::Client,
 ) -> Result<(), RuntimeError> {
-    let mut get_tip_request = ipc_client.mining.get_tip_request();
-    {
-        let mut context = get_tip_request
-            .get()
-            .get_context()
-            .map_err(|e| RuntimeError::ipc_call(IpcCallKind::MiningGetTip, e))?;
-        context.set_thread(ipc_client.thread.clone());
-        context.set_callback_thread(ipc_client.thread.clone());
-    }
-
-    let get_tip_response = get_tip_request
-        .send()
-        .promise
-        .await
-        .map_err(|e| RuntimeError::ipc_call(IpcCallKind::MiningGetTip, e))?;
-
-    let height: i32;
-    let mut hash: Vec<u8>;
-    {
-        let tip = get_tip_response
-            .get()
-            .map_err(|e| RuntimeError::ipc_call(IpcCallKind::MiningGetTip, e))?
-            .get_result()
-            .map_err(|e| RuntimeError::ipc_call(IpcCallKind::MiningGetTip, e))?;
-
-        height = tip.get_height();
-        hash = tip
-            .get_hash()
-            .map_err(|e| RuntimeError::ipc_call(IpcCallKind::MiningGetTip, e))?
-            .to_vec();
-        hash.reverse();
-    }
+    let tip = ipc_client.get_tip().await?;
 
     let proto = Event::new(PeerObserverEvent::IpcExtractor(ipc_extractor::Ipc {
-        ipc_event: Some(ipc_extractor::ipc::IpcEvent::BlockTip(BlockTip {
-            height,
-            hash,
-        })),
+        ipc_event: Some(ipc_extractor::ipc::IpcEvent::BlockTip(tip)),
     }))?;
 
     nats_client
